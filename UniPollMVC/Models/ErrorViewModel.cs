03e9b119599b7a5f1f388f6c15c3a04836f67682
Silverwizard80 <probetatesting@gmail.com>
2020-03-08 11:15:12 -0400

    Inital commit

---
+++ UniPollMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UniPollMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
